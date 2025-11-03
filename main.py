03405bb8bea10c699dcb5cc729dfdae946e2765c
Moku3956 <moku3956@icloud.com>
2025-11-03 19:12:45 +0900

    現在地取得

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import graph_data
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/api/get-route")
+def get_info(lat: float, lon: float):
+    print(f"{lat}, {lon}")
+    
+@app.get("/")
+def display(request: Request):
+    return templates.TemplateResponse("map.html", {"request": request})
+    
 
